3aabf28bf85e772f04699e910335c6fd32a2c46b
dugs <dugs@list.ru>
2022-12-07 22:50:39 +0300

    Task 1,2,3

---
+++ seminar1/Task3.cs
@@ -0,0 +1,13 @@
+﻿// на вход принимает число и выдаёт, является ли число чётным
+Console.WriteLine("Ввод числа");
+string number = Console.ReadLine()!;
+int num = int.Parse(number);
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
