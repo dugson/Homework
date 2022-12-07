964f14f051adfaf3b463a0c94291696dc87c6309
dugs <dugs@list.ru>
2022-12-07 22:03:15 +0300

    Task2

---
+++ seminar1/Task2.cs
@@ -0,0 +1,24 @@
+﻿// на вход три числа, какое больше 
+Console.WriteLine("Ввод первого числа");
+string number1 = Console.ReadLine()!;
+int num1 = int.Parse(number1);
+
+Console.WriteLine("Ввод второго числа");
+string number2 = Console.ReadLine()!;
+int num2 = int.Parse(number2);
+
+Console.WriteLine("Ввод третьего числа");
+string number3 = Console.ReadLine()!;
+int num3 = int.Parse(number3);
+
+int max = num1;
+
+if (max < num2)
+{
+    max = num2;
+}
+if (max < num3)
+{
+    max = num3;
+}
+Console.WriteLine(max);
