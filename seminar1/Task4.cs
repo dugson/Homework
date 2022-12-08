5a2d07d0f3441fc098910eb97e06429bbce3b250
dugs <dugs@list.ru>
2022-12-08 20:37:47 +0300

    task 1,2,3,4

---
+++ seminar1/Task4.cs
@@ -0,0 +1,22 @@
+﻿//на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+Console.WriteLine("Ввод числа");
+string number = Console.ReadLine()!;
+int num = int.Parse(number);
+int counter = 1;
+
+if (num > 0)
+{
+}
+else
+{
+    num = -num;
+}
+while (counter < num)
+{
+    counter = counter + 1;
+
+    if (counter % 2 == 0)
+    {
+        Console.Write(counter + "  ");
+    }
+}
