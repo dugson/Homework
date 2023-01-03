a54bf19f4437036e79551cefa3028b52fc7b470a
dugson <dugs@list.ru>
2023-01-03 19:46:08 +0200

    seminar 6

---
+++ seminar6/Task1.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите количество чисел");
+int countNumber = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Количество положительных чисел: " + CountPositiveNum(countNumber));
+
+int CountPositiveNum(int countNumber)
+{
+    int counter = 0;
+    for (int i = 0; i < countNumber; i++)
+    {
+        int number = int.Parse(Console.ReadLine()!);
+        if (number > 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
---
+++ seminar6/Task2.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите b1");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k1");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите b2");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k2");
+double k2 = double.Parse(Console.ReadLine()!);
+
+double[] pointCross = CrossPoint(b1, k1, b2, k2);
+Console.WriteLine("x = " + pointCross[0]);
+Console.WriteLine("y = " + pointCross[1]);
+
+double[] CrossPoint(double b1, double k1, double b2, double k2)
+{
+    double[] coordinates = new double[2];
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    coordinates[0] = x;
+    coordinates[1] = y;
+    return coordinates;
+}
